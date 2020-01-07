95b80641b226e046985af428e99ff18fb0e10678
Abby Kaemmerer <kaemmerer.abby@gmail.com>
2020-01-06 19:13:37 -0600

    Add new colors/play again button

--- index.js
+++ index.js
@@ -5,7 +5,17 @@ let pickedColor = pickColor();
 let colorDisplay = document.getElementById("colorDisplay");
 let messageDisplay = document.querySelector("#message");
 let h1 = document.querySelector("h1");
+let resetButton = document.querySelector("#reset");
 
+resetButton.addEventListener("click", function(){
+    colors = generateRandomColors(6);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for(let i = 0; i < squares.length; i++){
+        squares[i].style.backgroundColor = colors[i]
+    }
+    h1.style.backgroundColor = "#232323";
+})
 colorDisplay.textContent = pickedColor;
 
 for(let i = 0; i < squares.length; i++){
@@ -16,6 +26,7 @@ for(let i = 0; i < squares.length; i++){
 
         if(clickedColor === pickedColor){
             messageDisplay.textContent = "Correct!";
+            resetButton.textContent = "Play Again?"
             changeColors(clickedColor);
             h1.style.backgroundColor = clickedColor;     
         }
