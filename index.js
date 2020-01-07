8a387a9cfab6581dd6ceb9b9586c386148660360
Abby Kaemmerer <kaemmerer.abby@gmail.com>
2020-01-06 18:32:19 -0600

    Set up logic for color picker

--- index.js
+++ index.js
@@ -8,19 +8,37 @@ let colors = [
 ]
 
 let squares = document.querySelectorAll(".square");
-let pickedColor = colors[3];
+let pickedColor = pickColor();
 let colorDisplay = document.getElementById("colorDisplay");
+let messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 
 for(let i = 0; i < squares.length; i++){
     squares[i].style.backgroundColor = colors[i];
+    // eslint-disable-next-line func-names
     squares[i].addEventListener("click", function(){
         let clickedColor = this.style.backgroundColor;
-        if (clickedColor === pickedColor){
-            alert("Correct");
-            } else {
-                alert("Wrong!");
-            }
+
+        if(clickedColor === pickedColor){
+            messageDisplay.textContent = "Correct!";
+            changeColors(clickedColor);
+        }
+        else{
+            this.style.backgroundColor = "#232323";
+            messageDisplay.textContent = 'Try again';
+        }
     });
+}
+
+function changeColors(color){
+    for (let i = 0; i < colors.length; i++){
+        squares[i].style.backgroundColor = color;
+    }
+}
+
+function pickColor(){
+    let random = Math.floor(Math.random() * colors.length);
+    
+    return colors[random];
 }
